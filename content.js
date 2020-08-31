435d3dc0437eec913885e23001abc74b59e1616a
James Higgins <james.milan.higgins@gmail.com>
2020-08-31 11:13:44 -0700

    Only show for TSLA, and update image

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+
+// is yahoo on tsla?
+// check URL?
+var yahoo_url = window.location.href;
+if (yahoo_url.includes("TSLA")) {
   var parent = document.getElementsByClassName('D(ib) Mend(20px)')[1].innerText;
   if (parent.length > 0) {
     var spliced_string_price = parent.substring(0, parent.indexOf('+'));
@@ -9,4 +14,5 @@
     element.appendChild(document.createTextNode(' The Pre-Split Price would be: $' + pre_price));
     document.getElementById('quote-market-notice').appendChild(element);
     console.log("Pre Stock Price span added");
+}
 }
